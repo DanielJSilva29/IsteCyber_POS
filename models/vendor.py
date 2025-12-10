08c4abd65d78d542b9c3a424fb472798a40e9d43
Daniel Silva <83838310+DanielJSilva29@users.noreply.github.com>
2025-12-10 12:46:55 +0000

    Delete models directory

--- models/vendor.py
+++
@@ -1,7 +0,0 @@
-"""Modelo de Vendedor."""
-from .user import User
-
-class Vendor(User):
-    """Vendedor da loja."""
-    def __init__(self, username: str, email: str, password: str):
-        super().__init__(username=username, email=email, password=password, role="VENDOR")
